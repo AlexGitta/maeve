6bf0f9ba7c5b7e2925c2deafcf92c67a81bd0175
Alex <notalexevans42@gmail.com>
2025-11-05 21:01:53 +0000

    cased

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/maeve',
+  basePath: '/Maeve',
   images: {
     unoptimized: true,
     remotePatterns: [
